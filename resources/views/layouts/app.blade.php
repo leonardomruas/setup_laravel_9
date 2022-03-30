b002b74c75baeeb988659d34bf2b12b1ffd2c99f
Leonardo Messalla <leonardoitanhem@gmail.com>
2022-03-29 22:05:54 -0300

    Applying Tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>@yield('title')</title>
+  <title>@yield('title') - App Laravel 9</title>
+
+  <link rel="shortcut icon" href="{{ url('images/favicon.ico') }}" type="image/png">
+
+  <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-  <div class="app">
+<body class="bg-gray-50">
+  <div class="container mx-auto px-4 py-8">
     @yield('content')
   </div>
 </body>
